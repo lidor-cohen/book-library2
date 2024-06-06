2185bc5a1ccd19adf27f5a82a4b198f3edde130e
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-06-06 23:31:22 +0300

    changed card ui, added book class and support functions to add card to screen

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
+const itemsContainer = document.querySelector(".container");
+
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
+
+function createCard(book) {
+  let card = document.createElement("div");
+  card.classList.add("item");
+
+  console.log(book);
+
+  // Item Header Element Creation
+  let itemHeader = document.createElement("div");
+  let itemTitle = document.createElement("h2");
+  let itemDivider = document.createElement("hr");
+  let itemAuthor = document.createElement("h4");
+
+  itemHeader.classList.add("item-header-section");
+  itemTitle.classList.add("item-title");
+  itemAuthor.classList.add("item-author");
+  itemTitle.textContent = book.title;
+  itemAuthor.textContent = book.author;
+
+  itemHeader.appendChild(itemTitle);
+  itemHeader.appendChild(itemDivider);
+  itemHeader.appendChild(itemAuthor);
+
+  // Item Info Section Element Creation
+  let itemInfoSection = document.createElement("div");
+  let itemInfoPages = document.createElement("p");
+  let itemInfoRead = document.createElement("button");
+
+  itemInfoSection.classList.add("item-info-section");
+  itemInfoPages.classList.add("pages-text");
+  itemInfoRead.classList.add("read-btn");
+  itemInfoPages.textContent = book.pages + " pages";
+  itemInfoRead.textContent = true ? "Read" : "Not Read";
+
+  itemInfoSection.appendChild(itemInfoPages);
+  itemInfoSection.appendChild(itemInfoRead);
+
+  card.appendChild(itemHeader);
+  card.appendChild(itemInfoSection);
+
+  itemsContainer.appendChild(card);
+}
+
+let b = new Book("Harry Potter", "J.K. Rowling", 920, true);
+addBookToLibrary(b);
+
+createCard(myLibrary[0]);
