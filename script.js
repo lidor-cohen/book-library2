139c127bb1c322d5c001b86c6ea82b22f55ebd6c
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-06-06 16:45:50 +0300

    changed read text to button

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const myLibrary = [];
 
 function Book() {
-  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
